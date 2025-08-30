3290ad9f9dd61657172700dddd87a0452effda92
adhi0987 <adhithyashivakumar2511@gmail.com>
2025-08-30 17:41:20 +0530

    small error pointed due to typescript corrected it

--- frontend/iot-frontend/src/components/Devicelist.tsx
+++ frontend/iot-frontend/src/components/Devicelist.tsx
@@ -1,10 +1,9 @@
-import React, { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 function DeviceList() {
   const [devices, setDevices] = useState([]);
   const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(null);
-
+  const [error, setError] = useState<string | null>(null);
   useEffect(() => {
     const fetchDevices = async () => {
       try {
@@ -14,8 +13,14 @@ function DeviceList() {
         }
         const data = await response.json();
         setDevices(data.unique_devices);
-      } catch (e) {
-        setError(e.message);
+      } catch (e:unknown) {
+        if(e instanceof Error)
+        {
+
+            setError(e.message);
+        }else{
+            setError("unknown error has occurred");
+        }
       } finally {
         setLoading(false);
       }
