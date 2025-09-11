1afae7ade3e37ab054a995058bc6b605bb94e404
adhi0987 <adhithyashivakumar2511@gmail.com>
2025-09-11 21:09:35 +0530

    added the model trainig code

--- backend/database.py
+++ backend/database.py
@@ -1,8 +1,8 @@
 import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
-# from dotenv import load_dotenv
-# load_dotenv()
+from dotenv import load_dotenv
+load_dotenv()
 # The DATABASE_URL environment variable will be set in Render
 DATABASE_URL = os.environ.get("DATABASE_URL")
 if not DATABASE_URL:
